9319164e9b2bf42d53e9408a1e404c36ae83563f
Teon Banek <theongugl@gmail.com>
2014-07-31 19:51:02 +0200

    Created basic SDL window

--- Main.hs
+++ Main.hs
@@ -1,3 +1,27 @@
+import Prelude
+import qualified Graphics.UI.SDL as SDL
+import Foreign.C.String
+import Foreign.C.Types
+import Foreign.Marshal.Alloc (free)
 
+main :: IO ()
 main = do
-  putStrLn "Hello world!"
+  SDL.setMainReady
+  e <- SDL.init SDL.initFlagEverything
+  if e == 0 then do
+    putStrLn "SDL_init succesful"
+    w <- createWindow
+    SDL.delay 3000
+    SDL.destroyWindow w
+  else getSDLErrorString >>= putStrLn
+  SDL.quit
+
+getSDLErrorString :: IO String
+getSDLErrorString = SDL.getError >>= (\e -> SDL.clearError >> peekCString e)
+
+createWindow :: IO SDL.Window
+createWindow = do
+  t <- newCString "hasgel"
+  w <- SDL.createWindow t (CInt 0) (CInt 0) (CInt 800) (CInt 600) SDL.windowFlagOpenGL
+  free t
+  return w
