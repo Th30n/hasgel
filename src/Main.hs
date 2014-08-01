4d3622d2912eae09d32ef39b8bf8915bc3430842
Teon Banek <theongugl@gmail.com>
2014-08-01 17:21:10 +0200

    Added OpenGL rendering

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,8 @@ import Foreign.C.Types
 import Foreign.Marshal.Alloc (free)
 import Foreign.Ptr (nullPtr)
 import qualified Graphics.UI.SDL as SDL
+import qualified Graphics.Rendering.OpenGL as GL
+import Graphics.Rendering.OpenGL (($=))
 import Prelude
 
 main :: IO ()
@@ -17,7 +19,15 @@ main = do
     res <- runExceptT createWindow
     case res of
       Right w -> do
-        SDL.delay 3000
+        cr <- runExceptT $ createContext w
+        case cr of
+          Right c -> do
+            GL.clearColor $= GL.Color4 1.0 0.0 0.0 1.0
+            GL.clear [GL.ColorBuffer]
+            SDL.glSwapWindow w
+            SDL.delay 3000
+            SDL.glDeleteContext c
+          Left err -> putStrLn err
         SDL.destroyWindow w
       Left err -> putStrLn err
   else getSDLErrorString >>= putStrLn
@@ -35,3 +45,10 @@ createWindow = do
   if w == nullPtr
   then liftIO getSDLErrorString >>= throwError
   else return w
+
+createContext :: SDL.Window -> ExceptT String IO SDL.GLContext
+createContext w = do
+  c <- liftIO $ SDL.glCreateContext w
+  if c == nullPtr
+  then liftIO getSDLErrorString >>= throwError
+  else return c
