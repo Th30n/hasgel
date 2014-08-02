e7cbf079b51878e22fff650479d8f7000ef3486f
Teon Banek <theongugl@gmail.com>
2014-08-02 11:41:01 +0200

    Moved display creation to separate module. Created a basic loop.

---
+++ src/Hasgel/Display.hs
@@ -0,0 +1,81 @@
+-- | Provides access to rendering display
+module Hasgel.Display (
+  initDisplay,
+  quitDisplay,
+  Display,
+  renderDisplay,
+  destroyDisplay,
+  createDisplay,
+  getSDLErrorString
+) where
+
+import Control.Monad.Except
+import Foreign.C.String
+import Foreign.Marshal.Alloc (free)
+import Foreign.Ptr (nullPtr)
+import qualified Graphics.UI.SDL as SDL
+
+-- | Stores resources of a display, and provides functions for rendering
+-- and cleaning up.
+data Display a b = Display {
+  getWindow :: a, -- ^ Get underlying window implementation.
+  getContext :: b, -- ^ Get underlying context implementation.
+  renderDisplay :: IO () -> IO (), -- ^ Take an IO rendering action and render.
+  destroyDisplay :: IO () -- ^ Clean up the resources of this display.
+}
+
+-- | Initialize display system.
+-- Returns error string if failed.
+initDisplay :: ExceptT String IO ()
+initDisplay = do
+  --SDL.setMainReady
+  e <- liftIO $ SDL.init SDL.initFlagEverything
+  when (e /= 0) $ liftIO getSDLErrorString >>= throwError
+
+-- | Shut down display system.
+quitDisplay :: IO ()
+quitDisplay = SDL.quit
+
+getSDLErrorString :: IO String
+getSDLErrorString = SDL.getError >>= (\e -> SDL.clearError >> peekCString e)
+
+createWindow :: ExceptT String IO SDL.Window
+createWindow = do
+  v <- liftIO $ SDL.glSetAttribute SDL.glAttrContextMajorVersion 3
+  when (v /= 0) $ liftIO getSDLErrorString >>= throwError
+  v' <- liftIO $ SDL.glSetAttribute SDL.glAttrContextMinorVersion 3
+  when (v' /= 0) $ liftIO getSDLErrorString >>= throwError
+  cfs <- liftIO $
+    SDL.glSetAttribute SDL.glAttrContextFlags SDL.glContextFlagForwardCompatible
+  when (cfs /= 0) $ liftIO getSDLErrorString >>= throwError
+  prof <- liftIO $
+    SDL.glSetAttribute SDL.glAttrContextProfileMask SDL.glProfileCore
+  when (prof /= 0) $ liftIO getSDLErrorString >>= throwError
+  t <- liftIO $ newCString "hasgel"
+  w <- liftIO $ SDL.createWindow t 0 0 800 600 SDL.windowFlagOpenGL
+  liftIO $ free t
+  when (w == nullPtr) $ liftIO getSDLErrorString >>= throwError
+  return w
+
+createContext :: SDL.Window -> ExceptT String IO SDL.GLContext
+createContext w = do
+  c <- liftIO $ SDL.glCreateContext w
+  when (c == nullPtr) $ liftIO getSDLErrorString >>= throwError
+  return c
+
+-- | Creates a display window with context for rendering.
+createDisplay :: ExceptT String IO (Display SDL.Window SDL.GLContext)
+createDisplay = do
+  w <- createWindow
+  c <- createContext w
+  return Display {
+    getWindow = w,
+    getContext = c,
+    renderDisplay = \renderAction -> do
+      renderAction
+      SDL.glSwapWindow w,
+    destroyDisplay = do
+      SDL.glDeleteContext c
+      SDL.destroyWindow w
+      
+  }
--- src/Main.hs
+++ src/Main.hs
@@ -1,59 +1,39 @@
 module Main ( main ) where
 
 import Control.Monad.Except
-import Foreign.C.String
-import Foreign.Marshal.Alloc (free)
-import Foreign.Ptr (nullPtr)
 import qualified Graphics.UI.SDL as SDL
 import qualified Graphics.Rendering.OpenGL as GL
 import Graphics.Rendering.OpenGL (($=))
 import Prelude
 
+import Hasgel.Display
+
 main :: IO ()
 main = do
-  SDL.setMainReady
-  e <- SDL.init SDL.initFlagEverything
-  if e == 0 then do
-    res <- runExceptT createWindow
-    case res of
-      Right w -> do
-        cr <- runExceptT $ createContext w
-        case cr of
-          Right c -> do
-            GL.clearColor $= GL.Color4 1.0 0.0 0.0 1.0
-            GL.clear [GL.ColorBuffer]
-            SDL.glSwapWindow w
-            SDL.delay 3000
-            SDL.glDeleteContext c
-          Left err -> putStrLn err
-        SDL.destroyWindow w
-      Left err -> putStrLn err
-  else getSDLErrorString >>= putStrLn
-  SDL.quit
+  e <- runExceptT initDisplay
+  case e of
+    Right () -> do
+      dr <- runExceptT createDisplay
+      case dr of
+        Right d -> do
+          loop World { loopState = Continue, display = d }
+          destroyDisplay d
+        Left err -> putStrLn err
+    Left err -> putStrLn err
+  quitDisplay
 
-getSDLErrorString :: IO String
-getSDLErrorString = SDL.getError >>= (\e -> SDL.clearError >> peekCString e)
+data LoopState = Continue | Quit
+data WorldState a b = World {
+  loopState :: LoopState,
+  display :: Display a b
+}
 
-createWindow :: ExceptT String IO SDL.Window
-createWindow = do
-  v <- liftIO $ SDL.glSetAttribute SDL.glAttrContextMajorVersion 3
-  when (v /= 0) $ liftIO getSDLErrorString >>= throwError
-  v' <- liftIO $ SDL.glSetAttribute SDL.glAttrContextMinorVersion 3
-  when (v' /= 0) $ liftIO getSDLErrorString >>= throwError
-  cfs <- liftIO $
-    SDL.glSetAttribute SDL.glAttrContextFlags SDL.glContextFlagForwardCompatible
-  when (cfs /= 0) $ liftIO getSDLErrorString >>= throwError
-  prof <- liftIO $
-    SDL.glSetAttribute SDL.glAttrContextProfileMask SDL.glProfileCore
-  when (prof /= 0) $ liftIO getSDLErrorString >>= throwError
-  t <- liftIO $ newCString "hasgel"
-  w <- liftIO $ SDL.createWindow t 0 0 800 600 SDL.windowFlagOpenGL
-  liftIO $ free t
-  when (w == nullPtr) $ liftIO getSDLErrorString >>= throwError
-  return w
-
-createContext :: SDL.Window -> ExceptT String IO SDL.GLContext
-createContext w = do
-  c <- liftIO $ SDL.glCreateContext w
-  when (c == nullPtr) $ liftIO getSDLErrorString >>= throwError
-  return c
+loop :: WorldState a b -> IO ()
+loop w = case loopState w of
+  Continue -> do
+    renderDisplay (display w) $ do
+      GL.clearColor $= GL.Color4 1.0 0.0 0.0 1.0
+      GL.clear [GL.ColorBuffer]
+    SDL.delay 3000
+    loop w { loopState = Quit }
+  Quit -> return ()
