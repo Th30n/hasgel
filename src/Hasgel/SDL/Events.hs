6fe69c58e93aa1d6d9c4ebc5b1e3294bf0009aef
Teon Banek <theongugl@gmail.com>
2014-08-07 11:43:54 +0200

    Added basic wrapper for pollEvents

---
+++ src/Hasgel/SDL/Events.hs
@@ -0,0 +1,32 @@
+-- | This module corresponds to SDL 2.0 Input Events, Event Handling
+-- category in official documentation wiki.
+module Hasgel.SDL.Events (
+  -- * Data types and enumerations
+  SDL.Event(..),
+  -- * Functions
+  pollEvent,
+) where
+
+import Foreign.Marshal.Alloc (alloca)
+import Foreign.Storable (peek)
+
+import qualified Graphics.UI.SDL as SDL
+
+type Event = SDL.Event
+
+-- | Polls for currently pending events. Returns 'Nothing' if no 'Event'
+-- available. This is the preferred way of receiving events since it can
+-- be done inside the main loop and does not suspend the main loop while
+-- waiting for an event.
+--
+-- This function implicitly calls SDL_PumpEvents(), you can only call this
+-- function in the thread that set the video mode.
+pollEvent :: IO (Maybe Event)
+pollEvent = alloca $ \e -> do
+  r <- SDL.pollEvent e
+  if r == 0 then
+    return Nothing
+  else do
+    event <- peek e
+    return $ Just event
+  
--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,12 @@
 module Main ( main ) where
 
 import Control.Monad.Except
-import Foreign.Marshal.Alloc (alloca)
-import Foreign.Storable (peek)
-import qualified Graphics.UI.SDL as SDL
 import qualified Graphics.Rendering.OpenGL as GL
 import Graphics.Rendering.OpenGL (($=))
 import Prelude
 
 import Hasgel.Display
+import Hasgel.SDL.Events as MySDL
 
 main :: IO ()
 main = do
@@ -25,6 +23,7 @@ main = do
   quitDisplay
 
 data LoopState = Continue | Quit
+
 data WorldState a b = World {
   loopState :: LoopState,
   display :: Display a b
@@ -33,22 +32,13 @@ data WorldState a b = World {
 loop :: WorldState a b -> IO ()
 loop w = case loopState w of
   Continue -> do
-    event <- getEvent
+    event <- MySDL.pollEvent
     renderDisplay (display w) $ do
       GL.clearColor $= GL.Color4 1.0 0.0 0.0 1.0
       GL.clear [GL.ColorBuffer]
     case event of
       Nothing -> loop w { loopState = Continue }
-      Just e -> loop $ if SDL.eventType e == SDL.eventTypeQuit
-                then w { loopState = Quit }
-                else w { loopState = Continue }
+      Just e -> loop $ case e of 
+                  MySDL.QuitEvent _ _ -> w { loopState = Quit }
+                  _ -> w { loopState = Continue }
   Quit -> return ()
-
-getEvent :: IO (Maybe SDL.Event)
-getEvent = alloca $ \e -> do
-      r <- SDL.pollEvent e
-      if r == 0 then return Nothing
-      else do
-        event <- peek e
-        return $ Just event
-
