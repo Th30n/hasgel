95e2c6a722d9b079962ab38583bd282bedbbbe75
Teon Banek <theongugl@gmail.com>
2014-08-11 14:24:38 +0200

    Added simple color transitions based on current time

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,10 @@
 module Main ( main ) where
 
 import Control.Monad.Except
+import Data.Word (Word32)
 import qualified Graphics.Rendering.OpenGL as GL
 import Graphics.Rendering.OpenGL (($=))
+import Graphics.UI.SDL as SDL
 import Prelude
 
 import Hasgel.Display
@@ -16,7 +18,9 @@ main = do
       dr <- runExceptT createDisplay
       case dr of
         Right d -> do
-          loop World { loopState = Continue, display = d }
+          current <- SDL.getTicks
+          loop World { loopState = Continue, display = d,
+              currentTime = current }
           destroyDisplay d
         Left err -> putStrLn err
     Left err -> putStrLn err
@@ -26,19 +30,27 @@ data LoopState = Continue | Quit
 
 data WorldState a b = World {
   loopState :: LoopState,
-  display :: Display a b
+  display :: Display a b,
+  currentTime :: Word32 -- ^ Time in milliseconds
 }
 
 loop :: WorldState a b -> IO ()
-loop w = case loopState w of
+loop curw = case loopState curw of
   Continue -> do
     event <- MySDL.pollEvent
+    let w = handleEvents event curw
     renderDisplay (display w) $ do
-      GL.clearColor $= GL.Color4 1.0 0.0 0.0 1.0
+      let current = (fromIntegral (currentTime w) / 1000) :: GL.GLclampf
+      let r = 0.5 + 0.5 * sin current
+      let g = 0.5 + 0.5 * cos current
+      GL.clearColor $= GL.Color4 r g 0.0 1.0
       GL.clear [GL.ColorBuffer]
-    case event of
-      Nothing -> loop w { loopState = Continue }
-      Just e -> loop $ case e of 
-                  MySDL.QuitEvent _ _ -> w { loopState = Quit }
-                  _ -> w { loopState = Continue }
+    current <- SDL.getTicks
+    loop w { currentTime = current}
   Quit -> return ()
+
+handleEvents :: Maybe MySDL.Event -> WorldState a b -> WorldState a b
+handleEvents Nothing w = w
+handleEvents (Just e) w = case e of
+    MySDL.QuitEvent _ _ -> w { loopState = Quit }
+    _ -> w
