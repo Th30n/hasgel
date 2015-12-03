c9e462ac62e98152272c8f1f66f5056f0ef39cf9
Teon Banek <theongugl@gmail.com>
2015-12-03 23:14:52 +0100

    Benchmark loading custom model format

--- src/Hasgel/Mesh/Test/BenchOBJ.hs
+++ src/Hasgel/Mesh/Test/BenchOBJ.hs
@@ -3,13 +3,20 @@ module Main where
 import Control.DeepSeq (NFData (..))
 import Criterion.Main
 
-import Hasgel.Mesh.OBJ
+import Hasgel.Mesh
 
-instance NFData OBJ where
-  rnf obj = rnf (objVertices obj) `seq`
-            rnf (objNormals obj) `seq`
-            rnf (objUvs obj) `seq`
-            rnf (objFaces obj)
+instance NFData Mesh where
+  rnf mesh = rnf (meshVertices mesh) `seq`
+             rnf (meshNormals mesh) `seq`
+             rnf (meshUvs mesh) `seq`
+             rnf (meshFaces mesh)
+
+instance NFData Face where
+  rnf face = rnf (faceVertexIx face) `seq`
+             rnf (faceUvIx face) `seq`
+             rnf (faceNormalIx face)
 
 main :: IO ()
-main = defaultMain [bench "ring.obj" $ nfIO $ parseFile "ring.obj"]
+main = defaultMain [bgroup "mesh loading"
+                    [bench "ring.obj" $ nfIO $ loadObj "ring.obj",
+                     bench "ring.hmd" $ nfIO $ loadHmd "ring.hmd"]]
